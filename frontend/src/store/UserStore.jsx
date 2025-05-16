3f0a4320a387ebfac976553aa9f20a86e7ee928d
shubhampathak1 <pathakshubhu123@gmail.com>
2025-05-16 20:33:28 +0545

    apiurl updated

--- frontend/src/store/UserStore.jsx
+++ frontend/src/store/UserStore.jsx
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import axios from 'axios'
 
-const USER_API_URL = "http://localhost:5000/api/auth"
+const USER_API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth"
 
 
 axios.defaults.withCredentials = true;
